47bcbbef2a06accd28bca14b194af063c3a3c8f6
adityakotwal100 <adityakotwal100gmail.com>
2020-07-03 14:49:54 +0530

    .

--- mess-management/src/app/login/login.component.ts
+++ mess-management/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ComponentFactoryResolver } from '@angular/core';
+import { Component, OnInit, ViewChild, ComponentFactoryResolver, ViewChildren } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -11,13 +11,22 @@ export class LoginComponent implements OnInit {
   get_result : any;
   user_ID : string;
   password : string;
-
+  show : boolean = true;
   constructor (private http: HttpClient) { }
 
   ngOnInit(): void {}
 
   @ViewChild('userID', {static:true}) id_no;
   @ViewChild('psw', {static:true}) psw;
+  @ViewChild('psw1',{static:true}) psw1;
+  @ViewChild('cpw',{static:true}) cpw;
+  register_form(){
+    this.show = !this.show;
+  }
+  confirm_password() : void{
+    if(this.psw1.nativeElement.value != this.cpw.nativeElement.value)
+    alert('Passwords do not match.');
+  }
 
   validate(uname : string, psw : string) : boolean{
     var passw=  "^(?=.*[a-z]).{5,8}$";
@@ -47,9 +56,9 @@ export class LoginComponent implements OnInit {
     this.user_ID = this.id_no.nativeElement.value;
     this.password = this.psw.nativeElement.value;
 
-    if(this.validate(this.user_ID,this.password))
+    if(this.validate(this.user_ID,this.password)== true)
   {
-    this.http.get('http://192.168.1.101:3000/students/?format=json').subscribe(data => {
+    this.http.get('http://localhost:8000/students/?format=json').subscribe(data => {
       this.get_result= data;
       console.log(this.get_result);
       console.log(this.user_ID);
